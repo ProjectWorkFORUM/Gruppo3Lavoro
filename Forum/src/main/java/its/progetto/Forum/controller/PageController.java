ef219c7f3f0d01f5b567d178ee84b627cc6597e5
andrebisco <andreabiscaldi9@gmail.com>
2026-07-07 22:22:41 +0200

    creato i controller per la nagivazion e della pagina

--- Forum/src/main/java/its/progetto/Forum/controller/PageController.java
+++ Forum/src/main/java/its/progetto/Forum/controller/PageController.java
@@ -7,9 +7,49 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PageController {
-    @GetMapping("/home")
+    @GetMapping("/")
     public String landingPage( ) {
         return "Landing_page";
     }
+
+    @GetMapping("/login")
+    public String loginPage( ) {
+        return "Login_page";
+    }
+
+    @GetMapping("/register")
+    public String registerPage( ) {
+        return "Register_page";
+    }
+
+    @GetMapping("/backoffice")
+    public String backofficePage( ) {
+        return "Backoffice_page";
+    }
+
+    @GetMapping("/dashboard")
+    public String dashboardPage( ) {
+        return "Dashboard_page";
+    }
+
+    @GetMapping("/esperienze")
+    public String esperienzePage( ) {
+        return "Esperienze_page";
+    }
     
+    @GetMapping("/home")
+    public String homePage( ) {
+        return "Home_page";
+    }
+
+    @GetMapping("/profilo")
+    public String profiloPage( ) {
+        return "Profilo_page";
+    }
+
+    @GetMapping("/profilo-personale")
+    public String profiloPersonalePage( ) {
+        return "Profilo_personale_page";
+    }
+
 }
