70191c878f3c551bea5869c9999e7305fb3a46a2
andrebisco <andreabiscaldi9@gmail.com>
2026-07-07 10:05:58 +0200

    pagina login register e controller pagina

---
+++ Forum/src/main/java/its/progetto/Forum/controller/PageController.java
@@ -0,0 +1,15 @@
+package its.progetto.Forum.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+@Controller
+public class PageController {
+    @GetMapping("/home")
+    public String landingPage( ) {
+        return "Landing_page";
+    }
+    
+}
