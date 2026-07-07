a61d92b718a5bd0ef1435a1f636ee892dfdb43dd
Riccardo <riccardovigano21@gmail.com>
2026-07-07 09:06:14 +0200

    aggiunti getter and setter classe Esperienze

--- Forum/src/main/java/Model/Esperienze.java
+++ Forum/src/main/java/Model/Esperienze.java
@@ -29,4 +29,66 @@ public class Esperienze {
 
     @OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL)
     private Set<Categorie> categorie = new HashSet<>();
+
+    public Esperienze() {
+    }
+
+    public Esperienze(Long id, String titolo, String descrizione, Double prezzo, Set<Categorie> categorie) {
+        this.id = id;
+        this.titolo = titolo;
+        this.descrizione = descrizione;
+        this.prezzo = prezzo;
+        this.categorie = categorie;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitolo() {
+        return titolo;
+    }
+
+    public void setTitolo(String titolo) {
+        this.titolo = titolo;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+
+    public Double getPrezzo() {
+        return prezzo;
+    }
+
+    public void setPrezzo(Double prezzo) {
+        this.prezzo = prezzo;
+    }
+
+    public Set<Categorie> getCategorie() {
+        return categorie;
+    }
+
+    public void setCategorie(Set<Categorie> categorie) {
+        this.categorie = categorie;
+    }
+
+    @Override
+    public String toString() {
+        return "Esperienze{" +
+                "titolo='" + titolo + '\'' +
+                ", descrizione='" + descrizione + '\'' +
+                ", prezzo=" + prezzo +
+                ", id=" + id +
+                ", categorie=" + categorie +
+                '}';
+    }
 }
